a44521c3a0256b676f0ded42bc022622b9d9bdba
moog500nl <52237999+moog500nl@users.noreply.github.com>
2019-08-02 13:35:11 +0200

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -28,7 +28,7 @@ legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2,
 
 
 ## PNG output file
-dev.copy(png, file="plot2.png", height=480, width=480)
+dev.copy(png, file="plot3.png", height=480, width=480)
 
 ## close graphics device
 dev.off()
